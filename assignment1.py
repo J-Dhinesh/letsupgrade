30923594d24e0873f5e3836a9ee579e9e4e4d839
J-Dhinesh <124454674+J-Dhinesh@users.noreply.github.com>
2023-02-04 21:58:53 +0530

    day 3 assignment submitted

---
+++ assignment1.py
@@ -0,0 +1,9 @@
+fortune=int(input("Enter the fortune number :"))
+if (fortune==1):
+	print("today will be a lucky day")
+elif (fortune==2):
+	print("You will meet someone new and interesting")
+elif (fortune==3):
+	print("Watch out for obstacles in your path")
+else:
+	print("No such condition")
